a16bf865991abb053651bd8da9dcc578382a392a
Seanwang5868 <wangshuai5868@outlook.com>
2026-04-06 19:30:12 +0100

    second structure update

--- src/xpid/ss.py
+++ src/xpid/ss.py
@@ -1,13 +1,10 @@
-"""
-residue_ss.py (Formerly ss.py)
-Secondary Structure helper.
-"""
 import gemmi
 from typing import Dict, List, Tuple
 
-def build_index(structure: gemmi.Structure) -> Dict[str, List[Tuple[int, int, str, int]]]:
+def build_index(structure: gemmi.Structure) -> Dict[str, List[Tuple[int, int, str, str]]]:
     ss_index = {}
-    uid = 1
+    helix_uid = 1
+    
     def add(chain, start, end, code, u):
         if chain not in ss_index: ss_index[chain] = []
         ss_index[chain].append((start, end, code, u))
@@ -15,24 +12,37 @@ def build_index(structure: gemmi.Structure) -> Dict[str, List[Tuple[int, int, st
     if hasattr(structure, 'helices'):
         for h in structure.helices:
             try:
-                code = 'H'
-                if h.pdb_helix_class == 5: code = 'G'
-                elif h.pdb_helix_class == 3: code = 'I'
-                add(h.start.chain_name, h.start.res_id.seqid.num, h.end.res_id.seqid.num, code, uid)
-                uid += 1
+                code = h.pdb_helix_class.name
+                add(h.start.chain_name, h.start.res_id.seqid.num, h.end.res_id.seqid.num, code, str(helix_uid))
+                helix_uid += 1
             except: pass
             
     if hasattr(structure, 'sheets'):
-        for s in structure.sheets:
-            for st in s.strands:
+        for sheet in structure.sheets:
+            for strand in sheet.strands:
                 try:
-                    add(st.start.chain_name, st.start.res_id.seqid.num, st.end.res_id.seqid.num, 'E', uid)
-                    uid += 1
+                    packed_uid = str(sheet.name) + str("_") + str(strand.name) 
+                    
+                    sense = getattr(strand, 'sense', 0)
+                    # 根据 sense 赋予不同的 code
+                    if sense == 1:
+                        code = 'Ep'  # Parallel (平行)
+                    elif sense == -1:
+                        code = 'Ea'  # Antiparallel (反平行)
+                    else:
+                        code = 'E0'  # First strand (第一条链)
+                    
+                    add(strand.start.chain_name, 
+                        strand.start.res_id.seqid.num, 
+                        strand.end.res_id.seqid.num, 
+                        code, 
+                        packed_uid)
                 except: pass
+                
     return ss_index
 
-def get_info(chain_name: str, res_num: int, ss_index: Dict) -> Tuple[str, int]:
-    if chain_name not in ss_index: return ('C', -1)
+def get_info(chain_name: str, res_num: int, ss_index: Dict) -> Tuple[str, str]:
+    if chain_name not in ss_index: return ('C', '-1')
     for start, end, code, uid in ss_index[chain_name]:
         if start <= res_num <= end: return (code, uid)
-    return ('C', -1)
+    return ('C', '-1')
